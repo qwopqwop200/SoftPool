36f2f209db7c9add5f0d7580ad7ac1192c6efdf6
qwopqwop200 <64115820+qwopqwop200@users.noreply.github.com>
2021-01-20 20:51:01 +0900

    Update torch_softpool.py

--- torch_softpool.py
+++ torch_softpool.py
@@ -1,9 +1,9 @@
 import torch
 
 class SoftPooling1D(torch.nn.Module):
-    def __init__(self,pool_size,strides=None,padding=0,ceil_mode = False,count_include_pad = True):
+    def __init__(self,kernel_size,strides=None,padding=0,ceil_mode = False,count_include_pad = True):
         super(SoftPooling1D, self).__init__()
-        self.avgpool = torch.nn.AvgPool1d(pool_size,strides,padding,ceil_mode,count_include_pad)
+        self.avgpool = torch.nn.AvgPool1d(kernel_size,strides,padding,ceil_mode,count_include_pad)
     def forward(self, x):
         x_exp = torch.exp(x)
         x_exp_pool = self.avgpool(x_exp)
@@ -11,9 +11,9 @@ class SoftPooling1D(torch.nn.Module):
         return x/x_exp_pool
 
 class SoftPooling2D(torch.nn.Module):
-    def __init__(self,pool_size,strides=None,padding=0,ceil_mode = False,count_include_pad = True,divisor_override = None):
+    def __init__(self,kernel_size,strides=None,padding=0,ceil_mode = False,count_include_pad = True,divisor_override = None):
         super(SoftPooling2D, self).__init__()
-        self.avgpool = torch.nn.AvgPool2d(pool_size,strides,padding,ceil_mode,count_include_pad,divisor_override)
+        self.avgpool = torch.nn.AvgPool2d(kernel_size,strides,padding,ceil_mode,count_include_pad,divisor_override)
     def forward(self, x):
         x_exp = torch.exp(x)
         x_exp_pool = self.avgpool(x_exp)
@@ -21,9 +21,9 @@ class SoftPooling2D(torch.nn.Module):
         return x/x_exp_pool
     
 class SoftPooling3D(torch.nn.Module):
-    def __init__(self,pool_size,strides=None,padding=0,ceil_mode = False,count_include_pad = True,divisor_override = None):
+    def __init__(self,kernel_size,strides=None,padding=0,ceil_mode = False,count_include_pad = True,divisor_override = None):
         super(SoftPooling3D, self).__init__()
-        self.avgpool = torch.nn.AvgPool3d(pool_size,strides,padding,ceil_mode,count_include_pad,divisor_override)
+        self.avgpool = torch.nn.AvgPool3d(kernel_size,strides,padding,ceil_mode,count_include_pad,divisor_override)
     def forward(self, x):
         x_exp = torch.exp(x)
         x_exp_pool = self.avgpool(x_exp)
